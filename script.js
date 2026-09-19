59603f9f5f38a06c4323e693fc4331a26278dab5
Hiral <hiralchavda1803@gmail.com>
2026-09-19 13:15:10 +0530

    mockproject

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 let number = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+let score = 0;
 let highscore = 0;
+let guesses = [];
+let gameOver = false;
 
 
-document.getElementById("check").addEventListener("click", function () {
 
-    let guess = Number(document.getElementById("guess").value);
+document.querySelector(".check").addEventListener("click", function () {
+
+    if (gameOver) {
+        return;
+    }
+
+    let guess = Number(document.querySelector(".guess").value);
+
     if (!guess) {
 
-        document.getElementById("message").textContent = "Please Enter a Number!";
+        document.querySelector(".message").textContent = "Please Enter a Number!";
 
     }
 
-    else if (guess == number) 
-    {
-        document.getElementById("message").textContent = "🎉 Correct Number!";
-        document.getElementById("number").textContent = number;
+    else if (guess < 1) {
+
+        document.querySelector(".message").textContent ="Number must be between 1 and 20!";
+
+    }
+
+    else if (guess > 20) {
+
+        document.querySelector(".message").textContent ="Number must be between 1 and 20!";
+
     }
 })
